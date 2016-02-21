35cf708e52a0351197f6c2c1cbe667f7cab2a681
Alex Vargas <vlexaargas@gmail.com>
2016-02-20 22:46:49 -0500

    Add like model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160221031224) do
+ActiveRecord::Schema.define(version: 20160221033422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,16 @@ ActiveRecord::Schema.define(version: 20160221031224) do
 
   add_index "acts", ["user_id"], name: "index_acts_on_user_id", using: :btree
 
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "act_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "likes", ["act_id"], name: "index_likes_on_act_id", using: :btree
+  add_index "likes", ["user_id"], name: "index_likes_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email",               default: "", null: false
     t.string   "encrypted_password",  default: "", null: false
@@ -57,4 +67,6 @@ ActiveRecord::Schema.define(version: 20160221031224) do
   add_foreign_key "act_views", "acts"
   add_foreign_key "act_views", "users"
   add_foreign_key "acts", "users"
+  add_foreign_key "likes", "acts"
+  add_foreign_key "likes", "users"
 end
